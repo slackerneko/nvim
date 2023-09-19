cc94ff75f89d92e68f31c207c03a708af8346959
slackerneko <slackerneko@gmail.com>
2023-09-19 10:27:05 +0500

    added nvim-cmp

--- init.lua
+++ init.lua
@@ -16,20 +16,61 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     "tpope/vim-fugitive",
+    "rebelot/kanagawa.nvim",
     {
-        "sainnhe/sonokai",
-        config = function()
-            vim.g.sonokai_style = "shusia"
-            vim.g.sonokai_enable_italic = 1
-            vim.g.sonokai_better_performance = 1
-        end,
+        "hrsh7th/nvim-cmp",
     }
 })
+
+require("kanagawa").setup({
+    colors = {
+        theme = {
+            all = {
+                ui = {
+                    bg_gutter = "none"
+                }
+            }
+        }
+    }
+})
+
+local cmp = require 'cmp'
+cmp.setup {
+    mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+   --   elseif luasnip.expand_or_locally_jumpable() then
+   --     luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }
+}
 
 local opt = vim.opt
 
 opt.termguicolors = true
-vim.cmd([[ colorscheme sonokai ]])
+vim.cmd([[ colorscheme kanagawa-dragon ]])
 
 opt.number = true
 opt.relativenumber = true
